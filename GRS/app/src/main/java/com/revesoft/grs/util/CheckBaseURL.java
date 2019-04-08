6d6c50ffcadd46c470023e0a788bc9ba1577c7f2
sajid.khan <sajid.khan@revesoft.com>
2019-04-08 09:28:49 +0600

    Test

--- GRS/app/src/main/java/com/revesoft/grs/util/CheckBaseURL.java
+++ GRS/app/src/main/java/com/revesoft/grs/util/CheckBaseURL.java
@@ -7,8 +7,8 @@ public class CheckBaseURL {
 
     public String URL() {
         if (Constant.KEY_IS_DEVELOPMENT_BUILD) {
-            BASE_URL = "http://www.grs.gov.bd";
-         //   BASE_URL = "192.168.18.56:8080";
+//            BASE_URL = "http://www.grs.gov.bd";
+            BASE_URL = "http://fembosa.org/dls";
         } else {
             BASE_URL = "http://103.48.18.9:81";
             // BASE_URL = "http://192.168.18.170:8080";
